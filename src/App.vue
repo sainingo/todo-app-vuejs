7a62887b381aec62d42854c25ee3caa6bc10ef53
sainingo <nickmentor421@gmail.com>
2022-11-26 17:58:11 +0300

    Design mobile view

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,25 @@ watch(task, (newTask) => {
 
 <template>
 <div>
-  <div class="bg-">
-    <h1 class="text-2xl">TODO</h1>
-  <div>
-    <input type="text" placeholder="Create a new todo"/>
+  <div class="bg-bg-mobile-light h-[200px] bg-center bg-no-repeat bg-cover text-white" >
+    <div class="flex justify-between p-8">
+      <h1 class="text-3xl tracking-[.4em] font-semibold">TODO</h1>
+      <img src="./assets/images/ICON-MOON.SVG" height="30px" width="30px" alt="moon icon" />
+    </div>
+  <div class="flex">
+    <input type="text" placeholder="Create a new todo" class="text-center p-4 rounded w-[80%] mx-auto outline-none text-black"/>
   </div>
   </div>
+  <section>
+    <div class="w-[80%] mx-auto mt-[-30px] bg-white p-2 rounded border shadow-lg h-[500px]">
+      <div class="flex justify-between m-2 border-b-2 p-2">
+        <input type="checkbox" class="rounded-full border-none bg-gray-100"/>
+        <span class="w-full ml-4">Jog around the park</span>
+        <button>
+          <img src="./assets/images/ICON-CROSS.SVG" />
+        </button>
+      </div>
+    </div>
+  </section>
 </div>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,10 @@ module.exports = {
         'bg-mobile-dark': "url('/src/assets/images/BG-MOBILE-DARK.JPG')",
         'bg-desktop-light': "url('/src/assets/images/bg-desktop-light.jpg')",
         'bg-desktop-dark': "url('/src/assets/images/bg-desktop-dark.jpg')",
+        'moon-icon': "url('/src/assets/images/ICON-MOON.SVG')",
+        'sun-icon': "url('/src/assets/images/ICON-SUN.SVG')",
+        'cross-icon': "url('/src/assets/images/ICON-CROSS.SVG')",
+        'check-icon': "url('/src/assets/images/ICON-CHECK.SVG')"
       }
     },
   },
